7bec0a70d1ac6452a5b4c2e32a754555655de33a
Benjamin Ye <benjaminye.email@gmail.com>
2025-12-29 23:26:00 -0600

    add stats

--- readme.py
+++ readme.py
@@ -1,60 +1,145 @@
+from datetime import datetime
 import os
 import requests
 
 graphql_url = "https://api.github.com/graphql"
 
+STATS_QUERY = f"""
+query userInfo($login: String!) {{
+  user(login: $login) {{
+    name
+    login
+    commits: contributionsCollection(from: "{datetime.now().year}-01-01T00:00:00Z") {{
+      totalCommitContributions
+    }}
+    reviews: contributionsCollection {{
+      totalPullRequestReviewContributions
+    }}
+    repositoriesContributedTo(first: 1, contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) {{
+      totalCount
+    }}
+    pullRequests(first: 1) {{
+      totalCount
+    }}
+    mergedPullRequests: pullRequests(states: MERGED) {{
+      totalCount
+    }}
+    openIssues: issues(states: OPEN) {{
+      totalCount
+    }}
+    closedIssues: issues(states: CLOSED) {{
+      totalCount
+    }}
+    followers {{
+      totalCount
+    }}
+    repositoryDiscussions {{
+      totalCount
+    }}
+    repositoryDiscussionComments(onlyAnswers: true) {{
+      totalCount
+    }}
+    repositories(first: 100, ownerAffiliations: OWNER) {{
+      totalCount
+      nodes {{
+        name
+        stargazers {{
+          totalCount
+        }}
+      }}
+    }}
+  }}
+}}
+"""
+
+LANGUAGES_QUERY = """
+query userInfo($login: String!) {
+  user(login: $login) {
+    repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+      nodes {
+        name
+        languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+          edges {
+            size
+            node {
+              color
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
 
 def generate_readme(username: str, token: str, path: str = "README.md"):
-    languages = get_languages(username, token).items()
+    stats = get_stats(username, token)
+    languages = get_languages(username, token)
 
-    total_size = sum(size for _, size in languages)
     with open(path, "w", encoding="utf-8") as readme:
+
+        readme.write("## 📊 Stats\n")
+        readme.write("```\n")
+        readme.write(f"Total Stars:    {stats["stars"]}\n")
+        readme.write(f"Total Commits:  {stats["commits"]}\n")
+        readme.write(f"Total PRs:      {stats["prs"]}\n")
+        readme.write(f"Contributed to: {stats["contributed_to"]}\n")
+        readme.write("```\n")
+
+        readme.write("\n")
+
         readme.write("## 💻 Top Languages\n")
         readme.write("```\n")
-        for lang, size in list(languages)[:10]:
+        total_size = sum(size for _, size in languages.items())
+        for lang, size in list(languages.items())[:10]:
             percent = (size / total_size) * 100 if total_size > 0 else 0
             bar = percent_bar(percent)
             readme.write(f"{lang:<12} {bar} {percent:.2f}%\n")
         readme.write("```\n")
 
 
-def get_languages(username: str, token: str):
-    query = """
-    query userInfo($login: String!) {
-      user(login: $login) {
-        repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
-          nodes {
-            name
-            languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
-              edges {
-                size
-                node {
-                  color
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    """
-
+def get_stats(username: str, token: str):
     headers = {
         "Authorization": f"token {token}",
         "Content-Type": "application/json",
     }
 
-    payload = {"query": query, "variables": {"login": username}}
+    payload = {"query": STATS_QUERY, "variables": {"login": username}}
 
     response = requests.post(graphql_url, json=payload, headers=headers)
     response.raise_for_status()
 
-    res = response.json()
-    repo_nodes = res["data"]["user"]["repositories"]["nodes"]
+    data = response.json()["data"]["user"]
+
+    stats = {}
+
+    for repository in data["repositories"]["nodes"]:
+        stats["stars"] = stats.get("stars", 0) + repository["stargazers"]["totalCount"]
+
+    stats["commits"] = data["commits"]["totalCommitContributions"]
+    stats["prs"] = data["pullRequests"]["totalCount"]
+    stats["contributed_to"] = data["repositoriesContributedTo"]["totalCount"]
+
+    return stats
+
+
+def get_languages(username: str, token: str):
+    headers = {
+        "Authorization": f"token {token}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {"query": LANGUAGES_QUERY, "variables": {"login": username}}
+
+    response = requests.post(graphql_url, json=payload, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()["data"]["user"]["repositories"]["nodes"]
 
     languages = {}
-    for repo in repo_nodes:
+    for repo in data:
         edges = repo["languages"]["edges"]
         if not edges:
             continue
